54df64db3094fab29d6240030b3f8ec6dfa501f6
vx-clutch <owestness@gmail.com>
2025-03-12 19:31:34 -0400

    save

---
+++ iota/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define VERSION "alpha"
+#define YEAR 2025
+#define NAME "iota"
+
+#endif
--- iota/syslog/error.h
+++ iota/syslog/error.h
@@ -1,26 +1,68 @@
 // Copyright (C) 2025 vx-clutch ( owestness@gmail.com )
 // See end of file for extended copyright information.
+#ifndef EREROR_H
+#define EREROR_H
 
-package create
+#include "../config.h"
+#include "../parse_args.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// TODO:
-// 1 Function per language
+// ANSI color codes
+extern const char *RESET;
+extern const char *RED;
+extern const char *PURPLE;
+extern const char *WHITE;
+extern const char *GREEN;
+extern const char *YELLOW;
 
-import "iota/pkg/errx"
+void
+pfatal(const char *message);
 
-func Create(name string, lang string) error {
-  switch lang {
-  default:
-    errx.Errx(3, false, "incomplete feature")
-    break
-  }
-  return nil
-}
+void
+pfatalf(const char *format, ...);
 
-/* iota is an opinionated project init tool.
- * Copyright (C) 2025 vx-clutch
+#define perror __perror
+int
+__perror(const char *message);
+
+void
+perrorf(const char *format, ...);
+
+
+int
+pwarning(const char *message);
+
+void
+pwarningf(const char *format, ...);
+
+#define pdebug(desc, message) __pdebug(__LINE__, __FILE__, desc, message)
+#define pdebugf(desc, message, ...) __pdebugf(__LINE__, __FILE__, desc, message, ##__VA_ARGS__)
+
+int
+__pdebug(int line, const char *file, const char *desc, const char *message);
+
+int
+__pdebugf(int line, const char *file, const char *desc, const char *format, ...);
+
+void
+plog(int status, const char *message);
+
+void
+plogf(int status, const char *format, ...);
+
+#define INFO 0,
+#define OK 1,
+#define WARN 2,
+#define FAIL 3,
+
+#endif
+
+/* vtc is a simple and extensible compiler.
+ * Copyright (C) 2024 vx-clutch
  *
- * The file is part of iota.
+ * The file is part of vtc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -34,7 +76,7 @@ func Create(name string, lang string) error {
  * used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
@@ -46,3 +88,4 @@ func Create(name string, lang string) error {
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
