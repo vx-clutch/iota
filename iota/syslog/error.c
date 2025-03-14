c8d12950939908cc2b35283b9325f280970c6a7a
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2025-03-14 08:06:03 -0400

    Update error.c

--- iota/syslog/error.c
+++ iota/syslog/error.c
@@ -6,7 +6,7 @@
 int
 perror(const char *message)
 {
-  fprintf(stderr, "%s%s%s: %serror%s: %s\n", WHITE, COMPILER_NAME, RESET, RED,
+  fprintf(stderr, "%s%s%s: %serror%s: %s\n", WHITE, NAME, RESET, RED,
           RESET, message);
   return 0;
 }
@@ -16,7 +16,7 @@ perrorf(const char *format, ...)
 {
   va_list args;
   va_start(args, format);
-  fprintf(stderr, "%s%s%s: %serror%s: ", WHITE, COMPILER_NAME, RESET, RED,
+  fprintf(stderr, "%s%s%s: %serror%s: ", WHITE, NAME, RESET, RED,
           RESET);
   vfprintf(stderr, format, args);
   fprintf(stderr, "\n");
