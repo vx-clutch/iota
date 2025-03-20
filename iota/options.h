ca1b1ef4fe080702221451595c6747551ea91973
vx-clutch <owestness@gmail.com>
2025-03-20 18:19:20 -0400

    more options yay

--- iota/options.h
+++ iota/options.h
@@ -1,13 +1,29 @@
 #ifndef OPT_H
 #define OPT_H
 
+#include <cstdint>
 #include <stdio.h>
 #include <stdint.h>
+
+enum lang_t {
+    C,
+    CXX,
+    PYTHON,
+    DEFAULT,
+};
 
 typedef struct 
 {
     uint8_t verbose;
     uint8_t debug;
+    uint8_t git;
+    uint8_t l_bsd;
+    uint8_t l_gpl;
+    uint8_t l_mit;
+    uint8_t no_build;
+    uint8_t no_markdown;
+    uint8_t name;
+    lang_t language;
 } __options;
 
 extern __options options;
