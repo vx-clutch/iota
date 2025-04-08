05c36769ffc74ee95fccf83bc578327da1046d37
vx-clutch <owestness@gmail.com>
2025-04-08 19:05:27 -0400

    save

--- iota/init.c
+++ iota/init.c
@@ -8,11 +8,13 @@ tostring(lang_t lang)
   {
   case LANG_DEFAULT:
     return "";
+#define new(_src, ...) _src
 #define lang(name, src)                                                        \
   case LANG_##name:                                                            \
     return #name;
 #include "language.objects/clang.lo"
 #undef lang
+#undef new
   case LANG_COUNT:
     pfatalf("getext called with LANG_COUNT.");
   }
--- iota/init.h
+++ iota/init.h
@@ -2,18 +2,22 @@
 #define INIT_H
 
 typedef enum {
+#define new(src, ...) src
 #define lang(name, src) LANG_##name,
   LANG_DEFAULT = 0,
 #include "language.objects/clang.lo"
 #undef lang
+#undef new
   LANG_COUNT,
 } lang_t;
 
 static const char *LANG_SRC[LANG_COUNT] = {
-  [LANG_DEFAULT] = "",
+    [LANG_DEFAULT] = "",
+#define new(_src, ...) _src
 #define lang(name, src) [LANG_##name] = src,
 #include "language.objects/clang.lo"
 #undef lang
+#undef new
 };
 
 char *tostring(lang_t);
