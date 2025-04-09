c3086da690981ade782ec791b528447094961e07
vx-clutch <owestness@gmail.com>
2025-04-09 17:26:07 -0400

    save

--- iota/common/write.c
+++ iota/common/write.c
@@ -1,8 +1,18 @@
 #include "write.h"
 
+#include <stdio.h>
+#include <stdarg.h>
+#include "../syslog/error.h"
+
 int
 write(char *path, char *src, ...)
 {
+  va_list args;
+  va_start(args, src);
   FILE *fp;
+  fp = fopen(path, "w");
+  if (!fp) pfatalf("Failed to open %s.", path);
+  fprintf(fp, src, args);
+  va_end(args);
   return 0;
 }
